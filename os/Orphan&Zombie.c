667df28ba1970f8d255b5cce6eee215325bc0af6
sahil-gusain <100952277+sahil-gusain@users.noreply.github.com>
2022-09-16 10:42:01 +0530

    Add files via upload

---
+++ os/Orphan&Zombie.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+int main()
+{
+	// ZOMBIE PROCESS
+	int p =fork();
+	if(p!=0){
+	    printf("parent process exited\n");
+	    sleep(10);
+	}
+	else{
+	printf("Zombie process\n");
+	printf("child procces is still running \n ");
+	}
+	//ORPHAN PROCESS
+	//int p =fork();
+	//if(p!=0){
+	  //  printf("parent process exited\n");
+	    //exit(0);
+	//}
+	//else{
+	//printf("Zombie process\n");
+	//printf("child procces is still running \n ");
+	//}
+}
+	
